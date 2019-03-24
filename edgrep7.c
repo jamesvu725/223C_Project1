12c0de07e6473ea85bb3fec5ab63fb38fdd0d671
jamesvu725 <james.vu725@csu.fullerton.edu>
2019-03-24 00:29:03 -0700

    removed most of address

--- edgrep7.c
+++ edgrep7.c
@@ -30,33 +30,9 @@ void commands(void) {  unsigned int *a1;  int c;
     }  error(Q);
   }
 }
-unsigned int* address(void) {  int sign;  unsigned int *a, *b;  int opcnt, nextopand;  int c;
-  nextopand = -1;  sign = 1;  opcnt = 0;  a = dot;
-  do {
-    do c = getchr(); while (c==' ' || c=='\t');
-    if ('0'<=c && c<='9') {  peekc = c;  if (!opcnt)  { a = zero; }
-    } else switch (c) {
-      case '$':  a = dol;  /* fall through */
-      case '.':  if (opcnt) { error(Q); } break;
-      case '\'':
-        c = getchr();  if (opcnt || c<'a' || 'z'<c) { error(Q); }  a = zero;
-        do { a++; } while (a<=dol && names[c-'a']!=(*a&~01));  break;
-      case '?':  sign = -sign;  /* fall through */
-      case '/':
-        compile(c);  b = a;
-        for (;;) {
-          a += sign;
-          if (a<=zero) { a = dol; }  if (a>dol) { a = zero; }  if (execute(a)) { break; }  if (a==b)  { error(Q); }
-        }
-        break;
-      default:
-        if (nextopand == opcnt) {  a += sign;  if (a < zero || dol < a)  { continue; } /* error(Q); */ }
-        if (c!='+' && c!='-' && c!='^') {  peekc = c;  if (opcnt==0) { a = 0; }  return (a);  }
-        sign = 1;  if (c!='+') { sign = -sign; }  nextopand = ++opcnt;  continue;
-    }
-    sign = 1;  opcnt++;
-  } while (zero<=a && a<=dol);
-  error(Q);  /*NOTREACHED*/  return 0;
+unsigned int* address(void) { unsigned int *a; int c;
+  a = 0; c = getchr(); peekc = c; return (a); error(Q);
+  /*NOTREACHED*/  return 0;
 }
 int advance(char *lp, char *ep) {  char *curlp;  int i;
   for (;;) {
